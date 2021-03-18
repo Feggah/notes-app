6d6a511b7a21ac87f0c141a8fc57aec814c8f4a9
Feggah <gabidferreira9@gmail.com>
2021-03-18 06:55:53 -0300

    Notes repository implementation

---
+++ lib/infrastructure/notes/note_repository.dart
@@ -0,0 +1,130 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+import 'package:injectable/injectable.dart';
+import 'package:dartz/dartz.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:notes_app/domain/notes/i_note_repository.dart';
+import 'package:notes_app/domain/notes/note_failure.dart';
+import 'package:notes_app/domain/notes/note.dart';
+import 'package:notes_app/infrastructure/core/firestore_helpers.dart';
+import 'package:notes_app/infrastructure/notes/note_dtos.dart';
+import 'package:rxdart/rxdart.dart';
+
+@LazySingleton(as: INoteRepository)
+class NoteRepository implements INoteRepository {
+  final Firestore _firestore;
+
+  NoteRepository(this._firestore);
+
+  @override
+  Future<Either<NoteFailure, Unit>> create(Note note) async {
+    try {
+      final userDoc = await _firestore.userDocument();
+      final noteDto = NoteDto.fromDomain(note);
+
+      userDoc.noteCollection.document(noteDto.id).setData(
+            noteDto.toJson(),
+          );
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.message.contains("PERMISSION_DENIED")) {
+        return left(const NoteFailure.insufficientPermission());
+      } else {
+        return left(const NoteFailure.unexpected());
+      }
+    }
+  }
+
+  @override
+  Future<Either<NoteFailure, Unit>> delete(Note note) async {
+    try {
+      final userDoc = await _firestore.userDocument();
+      final noteId = note.id.getOrCrash();
+
+      userDoc.noteCollection.document(noteId).delete();
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.message.contains("PERMISSION_DENIED")) {
+        return left(const NoteFailure.insufficientPermission());
+      } else if (e.message.contains('NOT_FOUND')) {
+        return left(const NoteFailure.unableToUpdate());
+      } else {
+        return left(const NoteFailure.unexpected());
+      }
+    }
+  }
+
+  @override
+  Future<Either<NoteFailure, Unit>> update(Note note) async {
+    try {
+      final userDoc = await _firestore.userDocument();
+      final noteDto = NoteDto.fromDomain(note);
+
+      userDoc.noteCollection.document(noteDto.id).updateData(
+            noteDto.toJson(),
+          );
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.message.contains("PERMISSION_DENIED")) {
+        return left(const NoteFailure.insufficientPermission());
+      } else if (e.message.contains('NOT_FOUND')) {
+        return left(const NoteFailure.unableToUpdate());
+      } else {
+        return left(const NoteFailure.unexpected());
+      }
+    }
+  }
+
+  @override
+  Stream<Either<NoteFailure, KtList<Note>>> watchAll() async* {
+    final userDoc = await _firestore.userDocument();
+    userDoc.noteCollection
+        .orderBy('serverTimeStamp', descending: true)
+        .snapshots()
+        .map(
+          (snapshot) => right<NoteFailure, KtList<Note>>(
+            snapshot.documents
+                .map((doc) => NoteDto.fromFirestore(doc).toDomain())
+                .toImmutableList(),
+          ),
+        )
+        .onErrorReturnWith((error) {
+      if (error is PlatformException &&
+          error.message.contains('PERMISSION_DENIED')) {
+        return left(const NoteFailure.insufficientPermission());
+      } else {
+        return left(const NoteFailure.unexpected());
+      }
+    });
+  }
+
+  @override
+  Stream<Either<NoteFailure, KtList<Note>>> watchUncompleted() async* {
+    final userDoc = await _firestore.userDocument();
+    userDoc.noteCollection
+        .orderBy('serverTimeStamp', descending: true)
+        .snapshots()
+        .map(
+          (snapshot) => snapshot.documents
+              .map((doc) => NoteDto.fromFirestore(doc).toDomain()),
+        )
+        .map(
+          (notes) => right<NoteFailure, KtList<Note>>(
+            notes
+                .where(
+                  (note) =>
+                      note.todos.getOrCrash().any((todoItem) => !todoItem.done),
+                )
+                .toImmutableList(),
+          ),
+        )
+        .onErrorReturnWith((error) {
+      if (error is PlatformException &&
+          error.message.contains('PERMISSION_DENIED')) {
+        return left(const NoteFailure.insufficientPermission());
+      } else {
+        return left(const NoteFailure.unexpected());
+      }
+    });
+  }
+}
--- lib/injection.iconfig.dart
+++ lib/injection.iconfig.dart
@@ -9,8 +9,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:notes_app/infrastructure/auth/firebase_auth_facade.dart';
 import 'package:notes_app/domain/auth/i_auth_facade.dart';
-import 'package:notes_app/application/notes/note_watcher/note_watcher_bloc.dart';
+import 'package:notes_app/infrastructure/notes/note_repository.dart';
 import 'package:notes_app/domain/notes/i_note_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:notes_app/application/notes/note_watcher/note_watcher_bloc.dart';
 import 'package:notes_app/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:notes_app/application/auth/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -23,6 +25,8 @@ void $initGetIt(GetIt g, {String environment}) {
       () => firebaseInjectableModule.googleSignIn);
   g.registerLazySingleton<IAuthFacade>(
       () => FirebaseAuthFacade(g<FirebaseAuth>(), g<GoogleSignIn>()));
+  g.registerLazySingleton<INoteRepository>(
+      () => NoteRepository(g<Firestore>()));
   g.registerFactory<NoteWatcherBloc>(
       () => NoteWatcherBloc(g<INoteRepository>()));
   g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
